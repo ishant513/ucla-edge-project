c9ffb6f5de515d3883af63fe648a44d4b7bfda81
Ishan <ishantrip@gmail.com>
2020-06-29 13:08:02 -0700

    Use Localhost and Port

--- SwiftSocket/iOS Example/ViewController.swift
+++ SwiftSocket/iOS Example/ViewController.swift
@@ -5,8 +5,8 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var textView: UITextView!
   
-  let host = "apple.com"
-  let port = 80
+  let host = "localhost"
+  let port = 9000
   var client: TCPClient?
 
   override func viewDidLoad() {
